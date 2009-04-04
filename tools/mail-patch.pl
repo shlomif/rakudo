346e6e76f915f8df16d0103bd6de664c39cd88f7
Moritz Lenz <moritz@faui2k3.org>
2009-04-04 23:14:19 +0200

    mail-patch.pl: flesh out the --subject option; help message

--- tools/mail-patch.pl
+++ tools/mail-patch.pl
@@ -2,6 +2,7 @@
 use strict;
 use warnings;
 use MIME::Lite;
+use Getopt::Long;
 
 sub format_patch {
     my $what = shift;
@@ -28,18 +29,34 @@ sub parse_mail {
 }
 
 sub MAIN {
+
+    my $s = !GetOptions(
+            'subject=s' => \my $subject,
+            'help'      => \my $help,
+    );
+    if ($s || $help) {
+        usage();
+        exit $s;
+    }
     my @filenames = format_patch($ARGV[-1] || '-1');
+
+    if (@filenames > 0 && !$subject) {
+        unlink @filenames;
+        die "you need to supply a subject with the --subject option\n"
+            "when you want to submit multiple patches\n";
+    }
+
     my $header = parse_mail($filenames[0]);
     my $mail = MIME::Lite->new(
             From    => $header->{From},
             To      => 'Rakudo patches <moritz@faui2k3.org>',
-            Subject => $header->{Subject},
+            Subject => $subject || $header->{Subject},
             Type    => 'multipart/mixed',
     );
 
     $mail->attach(
             Type    => 'TEXT',
-            Data    => 'A sample e-mail',
+            Data    => "Patches submitted via toos/mail-patch.pl\n",
     );
 
     for (@filenames) {
@@ -51,7 +68,29 @@ sub MAIN {
         );
     }
 
-    $mail->send('smtp', 'faui2k3.org', Debug => 1);
+    $| = 1;
+    print "sending mail ...";
+    $mail->send('smtp', 'faui2k3.org');
+    print " done\n";
 }
 
 MAIN() unless caller;
+
+sub usage {
+    print <<"USAGE";
+Usage:
+    $0 [options] [WHAT]
+
+Options:
+    --help          This help message
+    --subject=subj  Set a subject for the mail. Mandatory when multiple
+                    patches are submitted. Otherwise defaults to the
+                    first line of the commit message
+
+Arguments:
+    WHAT            What patches to send. -2 means "the last two commits",
+                    a branch name means "all the difference to that branch".
+                    Defaults to -1 (last commit)
+
+USAGE
+}
